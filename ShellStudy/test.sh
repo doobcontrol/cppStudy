4839695cc152a594c108a7262108971bee6abf31
dooacontrol <dooacontrol@gmail.com>
2024-01-09 20:22:41 +0800

    Linux Special Variables

--- ShellStudy/test.sh
+++ ShellStudy/test.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-# Author : Zara Ali
-# Copyright (c) Tutorialspoint.com
-# Script follows here:
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
 
-echo "What is your name?"
-read PERSON
-echo "Hello, $PERSON"
+for TOKEN in $*
+do
+   echo $TOKEN
+done
